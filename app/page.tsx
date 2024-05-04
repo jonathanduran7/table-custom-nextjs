b64ddb80cb51fe2e6a79d5ad34b6044aec9ad3ae
jonathanduran7 <joniduran624@gmail.com>
2024-05-03 23:36:27 -0300

    remove imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 'use client'
-import { useEffect, useState } from "react";
-import CustomTable from "./components/table/Table";
+import { useState } from "react";
 import { columns } from "./data/columns";
 import { data as fakeData } from "./data/data";
-import useChecks from "./hooks/useChecks";
 import TableCompound from "./components/compound/Table";
 
 export default function Home() {
 
   const [data, setData] = useState(fakeData)
-  // const { checks, handleCheck, handleCheckAll, isItemChecked, initializeChecks } = useChecks();
 
   const styleRow = {
     '&:hover': {
@@ -22,25 +19,9 @@ export default function Home() {
     color: '#000',
   }
 
-  useEffect(() => {
-    // initializeChecks(data)
-  }, [data])
-
   return (
     <main>
-      <div
-        style={{ minHeight: '200px' }}
-      >
-        {/* <CustomTable
-          checkboxFunctions={{ isItemChecked, initializeChecks, checks, handleCheck, handleCheckAll}}
-          checkbox={true}
-          tableOptions={{ stickyHeader: true }}
-          stylesRow={styleRow}
-          stylesHeader={styleHeader}
-          columns={columns}
-          data={data}
-        /> */}
-
+      <div style={{ minHeight: '200px' }}>
         <TableCompound columns={columns} data={data} hasCheckboxes={true}>
           <TableCompound.Head stylesHeader={styleHeader} />
           <TableCompound.Body stylesRow={styleRow} />
