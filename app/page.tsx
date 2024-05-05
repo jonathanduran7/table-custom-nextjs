4c7540a2b0d5f0113ac19d53f1593ddcd5d24c10
jonathanduran7 <joniduran624@gmail.com>
2024-05-05 15:56:29 -0300

    files for styles table

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,17 @@ import { useState } from "react";
 import { columns } from "./data/columns";
 import { data as fakeData } from "./data/data";
 import TableCompound from "./components/compound/Table";
+import { styleHeader, styleRow } from "./styles.table";
 
 export default function Home() {
 
   const [data, setData] = useState(fakeData)
 
-  const styleRow = {
-    '&:hover': {
-      backgroundColor: '#eeeeee'
-    },
-  }
-
-  const styleHeader = {
-    backgroundColor: '#dcdcdc',
-    color: '#000',
-  }
-
   return (
     <main>
       <div style={{ minHeight: '200px' }}>
         <TableCompound columns={columns} data={data} hasCheckboxes={true} hasOrder={true}>
-          <TableCompound.Head stylesHeader={styleHeader} deafultOrder="asc"/>
+          <TableCompound.Head stylesHeader={styleHeader} deafultOrder="asc" />
           <TableCompound.Body stylesRow={styleRow} />
         </TableCompound>
       </div>
