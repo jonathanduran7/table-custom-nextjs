658f307d60b19402718b66b47d8c687e9bbca3f8
jonathanduran7 <joniduran624@gmail.com>
2024-05-18 22:37:22 -0300

    modify useOrdertable hook

--- app/hooks/useOrderTable.tsx
+++ app/hooks/useOrderTable.tsx
@@ -4,63 +4,63 @@ import { IColumn } from "../interface/column.interface";
 type Order = 'asc' | 'desc';
 
 export default function useOrderTable() {
-    const [ordersColumn, setOrderColumns] = useState<{ key: string, orderColumn: Order }[]>([]);
+  const [ordersColumn, setOrderColumns] = useState<{ key: string, orderColumn: Order }[]>([]);
 
-    const orderColumn = (keyColumn: string) => {
-        let newDirection = 'asc';
-        const newOrderColumns = ordersColumn.map(orderColumn => {
-            if (orderColumn.key === keyColumn) {
-                newDirection = orderColumn.orderColumn === 'asc' ? 'desc' : 'asc';
-                return {
-                    key: orderColumn.key,
-                    orderColumn: newDirection as Order
-                }
-            }
-            return orderColumn;
-        });
-        setOrderColumns(newOrderColumns);
+  const orderColumn = (keyColumn: string) => {
+    let newDirection = 'asc';
+    const newOrderColumns = ordersColumn.map(orderColumn => {
+      if (orderColumn.key === keyColumn) {
+        newDirection = orderColumn.orderColumn === 'asc' ? 'desc' : 'asc';
         return {
-            key: keyColumn,
-            orderColumn: newDirection as Order
+          key: orderColumn.key,
+          orderColumn: newDirection as Order
         }
-    }
-
-    const initialOrderColumns = useCallback((data: IColumn[]) => {
-        const newOrdersColumn = data.map(column => {
-            return {
-                key: column.key,
-                orderColumn: 'asc' as Order
-            }
-        }); setOrderColumns(newOrdersColumn);
-    }, [])
-
-    const getNestedValue = (obj: any, key: string): any => {
-        return key.split('.').reduce((nestedObj, part) => {
-            return nestedObj ? nestedObj[part] : undefined;
-        }, obj);
-    };
-
-    const changeOrder = (data: any[], keyColumn: string, orderColumn: Order) => {
-        const newData = data.sort((a, b) => {
-            const valueA = getNestedValue(a, keyColumn);
-            const valueB = getNestedValue(b, keyColumn);
-
-            if (valueA > valueB) {
-                return orderColumn === 'asc' ? 1 : -1;
-            }
-            if (valueA < valueB) {
-                return orderColumn === 'asc' ? -1 : 1;
-            }
-            return 0;
-        });
-
-        return newData;
-    };
-
+      }
+      return orderColumn;
+    });
+    setOrderColumns(newOrderColumns);
     return {
-        ordersColumn,
-        orderColumn,
-        initialOrderColumns,
-        changeOrder
+      key: keyColumn,
+      orderColumn: newDirection as Order
     }
+  }
+
+  const initialOrderColumns = useCallback((data: IColumn[]) => {
+    const newOrdersColumn = data.map(column => {
+      return {
+        key: column.key,
+        orderColumn: 'asc' as Order
+      }
+    }); setOrderColumns(newOrdersColumn);
+  }, [])
+
+  const getNestedValue = (obj: any, key: string): any => {
+    return key.split('.').reduce((nestedObj, part) => {
+      return nestedObj ? nestedObj[part] : undefined;
+    }, obj);
+  };
+
+  const changeOrder = (data: any[], keyColumn: string, orderColumn: Order) => {
+    const newData = data.sort((a, b) => {
+      const valueA = getNestedValue(a, keyColumn);
+      const valueB = getNestedValue(b, keyColumn);
+
+      if (valueA > valueB) {
+        return orderColumn === 'asc' ? 1 : -1;
+      }
+      if (valueA < valueB) {
+        return orderColumn === 'asc' ? -1 : 1;
+      }
+      return 0;
+    });
+
+    return newData;
+  };
+
+  return {
+    ordersColumn,
+    orderColumn,
+    initialOrderColumns,
+    changeOrder
+  }
 }
