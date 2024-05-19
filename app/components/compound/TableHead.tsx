dd9a5ef34071f167a540e8cfc2ce47dc3663efb5
jonathanduran7 <joniduran624@gmail.com>
2024-05-18 23:20:20 -0300

    modify interface in table head

--- app/components/compound/TableHead.tsx
+++ app/components/compound/TableHead.tsx
@@ -7,62 +7,62 @@ import { TableConfig } from "@/app/interface/table-config.interface";
 import { useTableContext } from "@/app/hooks/useTableContext";
 
 interface TableCompoundProps {
-    columns?: IColumn[]
-    stylesHeader?: IRow
-    tableConfig?: TableConfig
-    deafultOrder?: Order;
-    customOrder?: {
-        handleOrderColumn?: (key: string) => void
-        ordersColumn?: { key: string, orderColumn: Order }[]
-    }
+  columns?: IColumn[]
+  stylesHeader?: IRow
+  tableConfig?: TableConfig
+  deafultOrder?: Order;
+  customOrder?: {
+    handleOrderColumn: (key: string, order: 'asc' | 'desc') => Promise<unknown>
+    ordersColumn?: { key: string, orderColumn: Order }[]
+  }
 }
 
 type Order = 'asc' | 'desc';
 
 export function TableHead({ columns, stylesHeader, deafultOrder, customOrder, tableConfig }: TableCompoundProps) {
 
-    const { hasCheckboxes, hasOrder } = tableConfig || { hasCheckboxes: false, hasOrder: false };
-    const { handleCheckAll } = useContext(CheckContext);
-    const { handleOrderColumn, ordersColumn, getOrderColumn} = useTableContext()
+  const { hasCheckboxes, hasOrder } = tableConfig || { hasCheckboxes: false, hasOrder: false };
+  const { handleCheckAll } = useContext(CheckContext);
+  const { handleOrderColumn, ordersColumn, getOrderColumn, setData } = useTableContext()
 
-    const customHandleOrder = (keyColumn: string) => {
-      const orders = getOrderColumn(keyColumn);  
-      const response = customOrder.handleOrderColumn(keyColumn)
-      console.log('response', response)
-    }
+  const customHandleOrder = async (keyColumn: string) => {
+    const orders = getOrderColumn(keyColumn);
+    const response = await customOrder?.handleOrderColumn(keyColumn, orders.orderColumn)
+    setData(response as any[])
+  }
 
-    return (
-        <HeadMui>
-            <TableRow >
-                {
-                    hasCheckboxes &&
-                    <TableCell sx={stylesHeader}>
-                        <Checkbox
-                            color="primary"
-                            inputProps={{ 'aria-label': 'select all desserts' }}
-                            onChange={(event) => handleCheckAll(event.target.checked)}
-                        />
-                    </TableCell>
-                }
-                {columns?.map((column, index) => (
-                    <TableCell
-                        key={index}
-                        sx={stylesHeader}
-                    >
-                        <TableSortLabel
-                            active={hasOrder}
-                            direction={ordersColumn.find(orderColumn => orderColumn.key === column.key)?.orderColumn || 'asc'}
-                            onClick={() =>
-                                customOrder?.handleOrderColumn
-                                    ? customHandleOrder(column.key)                                    
-                                    : handleOrderColumn(column.key)
-                            }
-                        >
-                            {column.title}
-                        </TableSortLabel>
-                    </TableCell>
-                ))}
-            </TableRow>
-        </HeadMui>
-    )
+  return (
+    <HeadMui>
+      <TableRow >
+        {
+          hasCheckboxes &&
+          <TableCell sx={stylesHeader}>
+            <Checkbox
+              color="primary"
+              inputProps={{ 'aria-label': 'select all desserts' }}
+              onChange={(event) => handleCheckAll(event.target.checked)}
+            />
+          </TableCell>
+        }
+        {columns?.map((column, index) => (
+          <TableCell
+            key={index}
+            sx={stylesHeader}
+          >
+            <TableSortLabel
+              active={hasOrder}
+              direction={ordersColumn.find(orderColumn => orderColumn.key === column.key)?.orderColumn || 'asc'}
+              onClick={() =>
+                customOrder?.handleOrderColumn
+                  ? customHandleOrder(column.key)
+                  : handleOrderColumn(column.key)
+              }
+            >
+              {column.title}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </HeadMui>
+  )
 }
