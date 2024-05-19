65b8ef04fda5fe194c40ad5367c9a546c00da08b
jonathanduran7 <joniduran624@gmail.com>
2024-05-18 22:41:55 -0300

    modify table head

--- app/components/table/TableHead.tsx
+++ app/components/table/TableHead.tsx
@@ -1,38 +1,37 @@
 import { IColumn } from "@/app/interface/column.interface";
-import { TableRow, TableHead as HeadMui, TableCell, Checkbox} from "@mui/material";
+import { TableRow, TableHead as HeadMui, TableCell, Checkbox } from "@mui/material";
 import { IRow } from "./Table";
 
 interface Props {
-    columns: IColumn[];
-    stylesHeader?: IRow
-    checkbox?: boolean
-    checkboxFunctions?: {
-        isItemChecked: (id: number) => boolean
-        initializeChecks: (data: { id: number }[]) => void
-        handleCheckAll: (checked: boolean) => void
-    }
+  columns: IColumn[];
+  stylesHeader?: IRow
+  checkbox?: boolean
+  checkboxFunctions?: {
+    isItemChecked: (id: number) => boolean
+    initializeChecks: (data: { id: number }[]) => void
+    handleCheckAll: (checked: boolean) => void
+  }
 }
 
-export default function TableHead({ columns, stylesHeader, checkbox, checkboxFunctions}: Props) {
-    return (
-        <HeadMui>
-            <TableRow >
-                {
-                    checkbox &&
-                    <TableCell sx={stylesHeader}>
-                        <Checkbox
-                            color="primary"
-                            inputProps={{ 'aria-label': 'select all desserts' }}
-                            onChange={(_, c) => checkboxFunctions?.handleCheckAll(c)}
-                        />
-                    </TableCell>
-                }
-                
-                {columns.map((column, index) => (
-                    <TableCell key={index} sx={stylesHeader}>{column.title}</TableCell>
-                ))}
-            </TableRow>
-        </HeadMui>
+export default function TableHead({ columns, stylesHeader, checkbox, checkboxFunctions }: Props) {
+  return (
+    <HeadMui>
+      <TableRow >
+        {
+          checkbox &&
+          <TableCell sx={stylesHeader}>
+            <Checkbox
+              color="primary"
+              inputProps={{ 'aria-label': 'select all desserts' }}
+              onChange={(_, c) => checkboxFunctions?.handleCheckAll(c)}
+            />
+          </TableCell>
+        }
+        {columns.map((column, index) => (
+          <TableCell key={index} sx={stylesHeader}>{column.title}</TableCell>
+        ))}
+      </TableRow>
+    </HeadMui>
 
-    )
+  )
 }
