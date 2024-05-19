6309d3bc37140d0c18973037184a0ceb010e3a95
jonathanduran7 <joniduran624@gmail.com>
2024-05-18 22:39:02 -0300

    add function for get order column

--- app/context/table/table-context.tsx
+++ app/context/table/table-context.tsx
@@ -1,12 +1,15 @@
 import { IColumn } from "@/app/interface/column.interface";
 import { Dispatch, SetStateAction, createContext } from "react";
 
+type Order = 'asc' | 'desc'
+
 interface TableContextProps {
     dataTable: any[]
     setData: Dispatch<SetStateAction<any[]>>
     handleOrderColumn: (column: string) => void
     initialOrderColumns: (data: IColumn[]) => void
     ordersColumn: { key: string, orderColumn: 'asc' | 'desc' }[]
+    getOrderColumn:(keyColumn: string) => { key: keyColumn, orderColumn: Order}
 }
 
 export const TableContext = createContext<TableContextProps | null>(null)
